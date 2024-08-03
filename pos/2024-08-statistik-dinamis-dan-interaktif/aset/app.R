edddb62ba65a33f7519f75447f3efda3e3136cb2
jelajahstatid <jelajahstatid@gmail.com>
2024-08-03 21:55:35 +0200

    Pembaharuan blog

--- pos/2024-08-statistik-dinamis-dan-interaktif/aset/app.R
+++ pos/2024-08-statistik-dinamis-dan-interaktif/aset/app.R
@@ -1,14 +1,17 @@
 library(shiny)
+library(tidyverse)
 load(url("https://github.com/jennybc/gapminder/raw/main/data/gapminder.rdata"))
 
 ui <- fluidPage(
+  titlePanel("PDB per Kapita vs. Angka Harapan Hidup"),
   sliderInput(
     inputId = "tahun",
     label = "Tahun",
     min = 1952,
     max = 2007,
     value = 2007,
-    step = 5
+    step = 5,
+    sep = ""
   ),
   plotOutput("diagram_pencar")
 )
@@ -19,8 +22,30 @@ server <- function(input, output, session) {
     
     gapminder |> 
       filter(year == tahun_int) |> 
-      ggplot(aes(x = gdpPercap, y = lifeExp)) + 
-      geom_point()
+      ggplot(
+        aes(x = gdpPercap, y = lifeExp)
+      ) + 
+      geom_point(
+        aes(color = continent, size = pop),
+        alpha = .6
+      ) + 
+      scale_size(
+        range = c(5, 30),
+        guide = "none"
+      ) + 
+      scale_color_viridis_d(name = "Benua") + 
+      theme_minimal(base_size = 18) + 
+      theme(
+        legend.position = "bottom",
+        plot.title = element_text(face = "bold")
+      ) + 
+      xlim(c(200, 60000)) + 
+      ylim(c(20, 90)) + 
+      labs(
+        x = "PDB per kapita",
+        y = "Angka harapan hidup",
+        caption = "Data: Jenny Bryan dkk. / Github"
+      )
     
   })
 }
